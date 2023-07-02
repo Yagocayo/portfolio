3215f77cacd97d652342ff1c8aa904bceaa999ab
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-07-02 17:49:21 -0300

    atualizando projeto

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+// header //
+const mobile = document.getElementById('mobile')
+
+function togglemenu (){
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+
+mobile.addEventListener('click', togglemenu);
+
+
+
+// main // 
