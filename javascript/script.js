c144135983166fef8b8bc331827fc996513e90a8
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2023-07-02 22:31:03 -0300

    trabalhano no projeto

--- javascript/script.js
+++ javascript/script.js
@@ -3,8 +3,15 @@ const mobile = document.getElementById('mobile')
 
 function togglemenu (event){
     if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('active')
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+       event.currentTarget.setAttribute('aria-label','fechar menu' )
+    } else {
+       event.currentTarget.setAttribute('aria-label','abrir menu' )
+    }
 }
 
 mobile.addEventListener('click', togglemenu);
